930414fbcd33573b2fa9d4c3e81467d38eb6b00b
DomFriz <ryuuie@gmail.com>
2022-10-16 23:48:38 -0500

    first commit
    first commit to upload to PythonAnywhere

---
+++ crm/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Customer, Service
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ( 'cust_name', 'organization', 'role', 'bldgroom', 'address', 'city', 'state', 'zipcode',
+            'email', 'phone')
+
+class ServiceForm(forms.ModelForm):
+   class Meta:
+       model = Service
+       fields = ('cust_name', 'service_category', 'description', 'location', 'setup_time', 'cleanup_time', 'service_charge')
